d8127214b3a61b29cd4a1f3192fb680cc879e49a
Lineri <andrejmaskov14@gmail.com>
2024-10-25 09:03:19 +0300

    Clenup and reformat.

--- Assets/SomeAttributeAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿using BeardPhantom.RuntimeTypeCache;
-using System;
-
-[TypeCacheTarget]
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
-public class SomeAttributeAttribute : Attribute { }
